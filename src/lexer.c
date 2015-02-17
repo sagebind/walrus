b94911d3957aafa9a14ae189e9e67af268519ae5
coderstephen <me@stephencoakley.com>
2015-02-17 16:00:15 -0600

    Move print tokens to lexer

---
+++ src/lexer.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lexer.h"
+#include "tokens.h"
+
+/**
+ * Prints some tokens to standard output.
+ *
+ * @param tokens A stream of tokens to print.
+ */
+void lexer_print_tokens(TokenStream* tokens)
+{
+    // loop over each token in the stream
+    for (int i = 0; i < tokens->length; ++i) {
+        printf("%.2d ", tokens->tokens[i].line);
+
+        if (tokens->tokens[i].type == T_BOOLEAN_LITERAL) {
+            printf("BOOLEANLITERAL ");
+        } else if (tokens->tokens[i].type == T_CHAR_LITERAL) {
+            printf("CHARLITERAL ");
+        } else if (tokens->tokens[i].type == T_INT_LITERAL) {
+            printf("INTLITERAL ");
+        } else if (tokens->tokens[i].type == T_STRING_LITERAL) {
+            printf("STRINGLITERAL ");
+        }
+
+        printf("%s\r\n", tokens->tokens[i].lexeme);
+    }
+}
---
+++ src/lexer.h
@@ -0,0 +1,8 @@
+#ifndef WALRUS_LEXER_H
+#define WALRUS_LEXER_H
+
+#include "tokens.h"
+
+void lexer_print_tokens(TokenStream*);
+
+#endif
