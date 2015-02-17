a75ececc69ae805a873e94ccc0cb6f1f3030923b
coderstephen <me@stephencoakley.com>
2015-02-17 16:11:32 -0600

    Some functions we will need for the lexer

--- src/lexer.h
+++ src/lexer.h
@@ -3,6 +3,27 @@
 
 #include "tokens.h"
 
+/**
+ * Tokenizes source code read from a scanner context.
+ *
+ * @param  context The scanner context to read from.
+ * @return         A stream of tokens that represent the given source code.
+ */
+TokenStream* lexer_tokenize(ScannerState* context);
+
+/**
+ * Parses the next token from a scanner context.
+ *
+ * @param  context The scanner context to read from.
+ * @return         A token parsed from the scanner input.
+ */
+Token lexer_next(ScannerState*);
+
+/**
+ * Prints some tokens to standard output.
+ *
+ * @param tokens A stream of tokens to print.
+ */
 void lexer_print_tokens(TokenStream*);
 
 #endif
