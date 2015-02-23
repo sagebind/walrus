43d8844a87868ae16b475c656588e5a24f7d82ca
coderstephen <me@stephencoakley.com>
2015-02-23 16:42:16 -0600

    Fix fprintf order

--- src/error.c
+++ src/error.c
@@ -6,6 +6,6 @@
  */
 void throw_error(Error code, char* error, int line, int column)
 {
-    fprintf(stderr, "Error(%d) at line %d, column %d: %s\n", code, error, line, column);
+    fprintf(stderr, "Error(%d) at line %d, column %d: %s\n", code, line, column, error);
     exit(code);
 }
