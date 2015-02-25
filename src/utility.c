d6d8899912d1d22c17e9da4a0df78b1f74f154fb
TylerWilcock <tyler.l.wilcock@gmail.com>
2015-02-25 14:02:23 -0600

    Started returning proper token for keywords

--- src/utility.c
+++ src/utility.c
@@ -6,8 +6,27 @@
 /**
  * Checks to see if the input string is a reserved keyword.
  */
-bool isReserved(char* inString)
+bool is_reserved(char* inString)
 {
     return(inString=="boolean"||inString=="int"||inString=="break"||inString=="continue"||inString=="callout"||inString=="class"||inString=="if"||
             inString=="else"||inString=="true"||inString=="false"||inString=="for"||inString=="return"||inString=="void");
+}
+
+/**
+ * Creates a token based on the passed in keyword
+ */
+Token create_keyword_token(char** in_keyword, ScannerContext* context)
+{
+	Token token;
+	//check in_keyword against list of all keywords here; if a match is found, return the appropriate token	
+
+
+	//in_keyword didn't match any of the known keywords - return illegal token
+	token = token_create(
+			context->line,
+			context->column,
+			T_ILLEGAL,
+			in_keyword
+		);
+	return token;
 }
--- src/utility.h
+++ src/utility.h
@@ -2,12 +2,21 @@
 #define WALRUS_UTILITY_H
 
 #include <stdbool.h>
-
+#include "tokens.h"
+#include "scanner.h"
 /**
  * Checks to see if the input string is a reserved keyword.
  *
  * @param inputString The string to check.
  */
-bool isReserved(char* inString);
+bool is_reserved(char* inString);
+
+/**
+ * Creates a token based on the passed in keyword
+ *
+ * @param keyword The input keyword
+ * @param context The current context of the scanner
+ */
+Token create_keyword_token(char** in_keyword, ScannerContext* context);
 
 #endif
