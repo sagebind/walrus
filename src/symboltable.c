2b78b9b05d6a7966b00f8f4639e510ec2c9fdfb9
AlexLaurent11 <LaurentAM03@uww.edu>
2015-04-29 17:31:21 -0500

    hash stuff

--- src/symboltable.c
+++ src/symboltable.c
@@ -1,10 +1,24 @@
 #include <stdlib.h>
 #include <string.h>
+#include "hashmap.h"
+int scope = 0;
 
+hashmap[50] scopes;
+hashmap activeTable;
 void initializeScope(){
-	
+	scopes[scope] = activeTable;
+	scope++;
+	if(scope <= 50){
+	activeTable = scopes[scope];
+	}
+	/*else allocate more space, but who needs more than 50 scope in decaf?*/
 }
 
 void finalizeScope(){
-	
+	scopes[scope] = activeTable;
+	if(scope >= 0){
+	scope--;
+	activeTable = scopes[scope];
+	}
+	/*else probably some kind of error*/
 }
